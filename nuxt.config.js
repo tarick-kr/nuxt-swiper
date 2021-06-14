fa2c490bf18aab324b4b82176220a73a29203295
tarick-kr <tsg-kr@ukr.net>
2021-06-14 23:51:29 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,27 +4,27 @@ export default {
    ** Doc: https://nuxtjs.org/api/configuration-mode
    */
   mode: "universal",
-  
+
   /*
    ** Headers of the page
    ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
    */
   head: {
-    title: "Nuxt.js starter for CSB",
+    title: "Nuxt.js starter",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: "Official Nuxt.js starter for CodeSandBox"
+        content: "Nuxt.js swiper, Infinite Scroll, Counter"
       }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  
+
   css: ["swiper/swiper-bundle.css"],
-  
+
   /*
    ** Plugins to load before mounting the App
    ** Doc: https://nuxtjs.org/guide/plugins
@@ -33,12 +33,12 @@ export default {
     { src: "@/plugins/infiniteScroll.js", mode: "client" },
     { src: "@/plugins/swiper.js", mode: "client" }
   ],
-  
+
   /*
    ** Nuxt.js modules
    ** Doc: https://nuxtjs.org/guide/modules
    */
-  modules: ["@nuxtjs/axios", "@nuxtjs/vuetify", "./codesandbox"].concat(
+  modules: ["@nuxtjs/axios", "@nuxtjs/vuetify"].concat(
     process.env.NODE_ENV === "production"
       ? [
         [
@@ -50,14 +50,14 @@ export default {
       ]
       : []
   ),
-  
+
   /*
    ** HTTP module configuration
    */
   http: {
     // See https://http.nuxtjs.org/api/#options
   },
-  
+
   /*
    ** Build configuration
    ** Doc: https://nuxtjs.org/api/configuration-build
